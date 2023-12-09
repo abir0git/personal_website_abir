7424def9dfa09531e7a1dcb36199c9f407a0d0e1
Abir <royabirlebachun0@gmail.com>
2023-12-09 12:38:28 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,10 @@
 import Navbar from "./Navbar";
-import Home_body from "./Home_body";
 
 const Home = () => 
 {
     return(
-        <div data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+        <div>
             <Navbar></Navbar>
-            <Home_body></Home_body>
         </div>
     )
 }
